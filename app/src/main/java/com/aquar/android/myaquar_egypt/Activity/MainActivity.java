0db7b35e0b441e83f9d6b1de39141162dc0ef6e3
abdelrahmanMagdi <abdelrahman.magdy95@hotmail.com>
2019-02-07 18:11:17 +0200

    first commit

---
+++ app/src/main/java/com/aquar/android/myaquar_egypt/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.aquar.android.myaquar_egypt.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aquar.android.myaquar_egypt.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/aquar/android/myaquar_egypt/Utils/ConstantsUrl.java
@@ -0,0 +1,12 @@
+package com.aquar.android.myaquar_egypt.Utils;
+
+/**
+ * Created by aswany on 2/7/19.
+ */
+
+public class ConstantsUrl {
+
+    public static final String BasicURL = "http:";
+    public static final String verifyRegistration = BasicURL+"http:";
+
+}
