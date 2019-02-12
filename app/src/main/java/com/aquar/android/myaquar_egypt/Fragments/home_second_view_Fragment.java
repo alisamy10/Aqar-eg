69034ecd5a5b5feaee9ad829422eda12bcd0cf82
ebrahemkofe <44553700+ebrahemkofe@users.noreply.github.com>
2019-02-12 15:09:15 +0200

    add home second view .

---
+++ app/src/main/java/com/aquar/android/myaquar_egypt/Fragments/home_second_view_Fragment.java
@@ -0,0 +1,39 @@
+package com.aquar.android.myaquar_egypt.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aquar.android.myaquar_egypt.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class home_second_view_Fragment extends Fragment {
+
+
+    public home_second_view_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home_second_view_, container, false);
+
+        return view;
+
+
+
+
+    }
+
+}
