c96e2287dff71f11a89f5df61396eafc08f44bed
magdy0000 <magdy.mahmoud0000@outlook.com>
2019-02-17 02:53:19 +0200

    h

---
+++ app/src/main/java/com/aquar/android/myaquar_egypt/Model/FavouriteModelList.java
@@ -0,0 +1,25 @@
+package com.aquar.android.myaquar_egypt.Model;
+
+
+
+public class FavouriteModelList {
+
+
+    public int imageProject ;
+    public   String nameProject ;
+    public   String locationProject ;
+
+
+    public FavouriteModelList () {}
+
+
+    public FavouriteModelList(int imageProject, String nameProject, String locationProject) {
+        this.imageProject = imageProject;
+        this.nameProject = nameProject;
+        this.locationProject = locationProject;
+    }
+
+
+
+
+}
