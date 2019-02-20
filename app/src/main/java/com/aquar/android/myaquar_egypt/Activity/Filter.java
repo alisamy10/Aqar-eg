0c22e70d676bac6980268500ebe8f4ebf834c9f2
magdy0000 <magdy.mahmoud0000@outlook.com>
2019-02-20 20:03:36 +0200

    uhgkjhk

--- app/src/main/java/com/aquar/android/myaquar_egypt/Activity/Filter.java
+++ app/src/main/java/com/aquar/android/myaquar_egypt/Activity/Filter.java
@@ -10,8 +10,8 @@ import android.widget.Toast;
 import com.aquar.android.myaquar_egypt.R;
 
 public class Filter extends AppCompatActivity {
-    public RadioGroup rg1,rg2;
-    public RadioButton rb1,rb2,rb3,rb4;
+    public RadioGroup rg1,rg2  , rg3;
+    public RadioButton rb1,rb2,rb3,rb4 , rb5 ;
 
 
     @Override
@@ -19,11 +19,14 @@ public class Filter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
         rg1=findViewById(R.id.radiogroup1);
+        rg3=findViewById(R.id.radiogroup3);
         rg2=findViewById(R.id.radiogroup2);
         rb1=findViewById(R.id.one);
         rb2=findViewById(R.id.two);
         rb3=findViewById(R.id.three);
         rb4=findViewById(R.id.four);
+        rb5 = findViewById(R.id.five);
+
 
        rb1.setOnClickListener(new View.OnClickListener() {
            @Override
@@ -38,8 +41,33 @@ public class Filter extends AppCompatActivity {
 
 
 
+
+
+
            }
        });
+
+        rb5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                rg1.clearCheck();
+                rg2.clearCheck();
+                rg3.clearCheck();
+
+
+                if (rb3.isChecked())
+                    rb3.setChecked(false);
+                rb1.setChecked(true);
+
+
+                rb5.setChecked(true);
+
+
+
+
+            }
+        });
 
        rb3.setOnClickListener(new View.OnClickListener() {
            @Override
