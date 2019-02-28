893bbc2f4642e0b8c04a2b0a26301e0b0938423b
ebrahemkofe <44553700+ebrahemkofe@users.noreply.github.com>
2019-02-28 22:14:23 +0200

    event details

---
+++ app/src/main/java/com/aquar/myaquar_egypt/Activity/EventsAndNewsDetails.java
@@ -0,0 +1,15 @@
+package com.aquar.myaquar_egypt.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aquar.myaquar_egypt.R;
+
+public class EventsAndNewsDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_and_news_details);
+    }
+}
