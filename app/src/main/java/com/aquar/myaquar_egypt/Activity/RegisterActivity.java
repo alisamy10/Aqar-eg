e3c73ab0b871ec194e7c0666887a744e33eff301
magdy0000 <magdy.mahmoud0000@outlook.com>
2019-03-11 16:44:44 +0200

    t

--- app/src/main/java/com/aquar/myaquar_egypt/Activity/RegisterActivity.java
+++ app/src/main/java/com/aquar/myaquar_egypt/Activity/RegisterActivity.java
@@ -143,18 +143,13 @@ private AlertDialog dialog1;
 
                         Log.d("RegisterResponse", response.toString());
                         String userOBJSTR = gson.toJson(resPOJO.getUserInfo());
-//
-//                        if(userOBJSTR.equals("null")){
-//
-//                            Toast.makeText(RegisterActivity.this, "This e-mail is already taken ", Toast.LENGTH_SHORT).show();
-//
-//                        }else {
+
 
 
                                      mySharedPreference.setUserToken(resPOJO.getUserInfo().getToken());
                                    mySharedPreference.setUserOBJ(userOBJSTR);
                                     startActivity(new Intent(RegisterActivity.this, MainActivity.class));
-//                        }
+
                     }
 
                     @Override
